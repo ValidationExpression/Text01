6cdaaa5835d245bf45d348479b5494217b2933a1
ValidationExpression <118657302+ValidationExpression@users.noreply.github.com>
2023-02-24 22:26:23 +0800

    页面跳转实现

---
+++ 页面跳转-example/miniprogram/pages/helloworld/helloworld.js
@@ -0,0 +1,70 @@
+// index.js
+//获取小程序实例(app.js)
+const app = getApp()
+Page({
+    data:{
+        msg:"hello world...",
+        vid:"123",
+        list:[{
+            name:"李华",
+            age:14,
+            id:1,
+            tags:["娱乐","时间"]
+        },{
+            name:"李芳",
+            age:17,
+            id:2,
+            tags:["世界","国际"]
+        },{
+            name:"挽歌",
+            age:21,
+            id:3,
+            tags:["未来","科技"]
+        }]
+    },
+    onload:function(options){
+        //再次初始化 
+        //页面创建时执行
+    },
+    onShow:function(options){
+        //页面出现在前台时执行
+
+    },
+    onReady:function(options){
+        //页面渲染完成时执行
+        console.log("Ready-小程序进入时间戳",app.globalData.times)
+        console.log("取页面数据(data)",this.data.msg)
+        setTimeout(_=>{
+            console.log("取改变后页面数据(data)",this.data.msg)
+        },2000)
+        //在此因为对app.js实例化了，所以可以直接调用其中的方法。
+        app.getUInfo()
+
+        //含义设置时间，2秒时修改data值为“你好...”,(修改那个值就set...)
+        setTimeout(_=>{
+            this.setData({
+                msg:"你好..."
+            })
+        },1000)
+    },
+    onHide:function(options){
+        //页面前台变后台时执行
+    },
+    onUnload:function(options){
+        //页面销毁时执行
+    },
+    //事件声明
+    handlClickEven(event){
+        const pap = event.currentTarget.dataset
+        console.log("点击了...数据",pap)
+    },
+
+    //跳转到不同页面实现
+    handlClickToEven(){
+        wx.navigateTo({
+            //路径
+            url:"/pages/news/index"
+        })
+
+    }
+})
